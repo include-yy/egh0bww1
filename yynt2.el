5f1fa418b8f922d6c758d18a9c6ff22759691384
include-yy <969041171@qq.com>
2023-07-08 17:51:23 +0900

    refactor head/postamles

--- yynt2.el
+++ yynt2.el
@@ -1,48 +1,189 @@
-;; -*- lexical-binding: t; -*-
+;; yynt2.el -- yynt.el 的改进版 -*- lexical-binding: t; -*-
 
+;;;; comment
+
+;; 相比于 yynt.el 使用 advice 修改 ox-html 而言使用了新的后端，不影响 ox-html 默认行为
+;; 添加了一些功能：
+;; - 路径运算
+;; - 生成 rss
+;; - 区别于 org-publish 的项目工具
+;; - ...
+
+;; 请使用 `eval-buffer' 加载 yynt，且保证 yynt.el 在项目根目录上
+
+;;; requests
+;; 加载自带的 htmlize
+;; 来自 https://github.com/hniksic/emacs-htmlize
 (require 'htmlize (expand-file-name "./htmlize.el"))
+;; 加载魔改版 html 后端
 (load-file (expand-file-name "./ox-yyhtml.el"))
 
+;;;; code
+
+;; 项目根目录
 (defvar yynt-basedir (expand-file-name "./"))
 
-;; https://sachachua.com/blog/2023/01/adding-a-custom-header-argument-to-org-mode-source-blocks-and-using-that-argument-during-export/
-;; (setq org-babel-exp-code-template "#+begin_src %lang :summary %summary\n%body\n#+end_src")
-;; (defun t-org-html-src-block (src-block _contents info)
-;;   (let* ((result (org-html-src-block src-block _contents info))
-;;          (block-info
-;;           (org-with-point-at (org-element-property :begin src-block)
-;;             (org-babel-get-src-block-info)))
-;;          (summary (assoc-default :summary (elt block-info 2))))
-;;     (if (member summary '("%summary" ""))
-;;         result
-;;       (format "<details><summary>%s</summary>%s</details>"
-;;               summary
-;;               result))))
-;; (with-eval-after-load 'ox-html
-;;   (map-put!
-;;    (org-export-backend-transcoders (org-export-get-backend 'html))
-;;    'src-block 't-org-html-src-block))
+;; 一些全局资源目录
+;; 如果有新的路径，在此添加
+(defconst yynt-d-css "css")
+(defconst yynt-d-img "img")
+(defconst yynt-d-js  "js")
 
+;; 一些扩展名，使用变量以免写错
+(defconst yynt-HTML "html")
+(defconst yynt-HTM  "htm")
+(defconst yynt-ORG  "org")
+(defconst yynt-XML  "xml")
+(defconst yynt-JS   "js")
+(defconst yynt-CSS  "css")
+(defconst yynt-PPT  "pptx?")
+(defconst yynt-JPG  "je?pg")
+(defconst yynt-PNG  "png")
+(defconst yynt-SVG  "svg")
+
+;; 一些全局的插入函数
 (defun yynt-imgattr ()
   "插入用于头图和尾图的 CSS"
   (interactive)
   (insert "#+ATTR_HTML: :class top-down-img"))
-
 (defun yynt-headimg ()
   "插入头部图片所需内容"
   (interactive)
   (insert "#+NAME: headimg\n")
   (yynt-imgattr))
-
 (defun yynt-tailimg ()
   "插入尾部图片所需内容"
   (interactive)
   (insert "#+NAME: tailimg\n")
   (yynt-imgattr))
-;;; post
-(defun yynt-post-postamble (_info)
+
+;;; file api
+;; 提供一些方便使用的文件操作 api
+;; 参考了 f.el: https://github.com/rejeep/f.el
+(defun yynt-fabsp (fname)
+  "判断路径是否为绝对路径"
+  (file-name-absolute-p fname))
+(defun yynt-frelap (fname)
+  "判断路径是否为相对路径"
+  (not (yynt-fabsp fname)))
+(defun yynt-frela (fname &optional dir)
+  "获取路径的相对路径名
+内部调用了 `file-relative-name'"
+  (file-relative-name fname dir))
+(defun yynt-fexpand (path &optional dir)
+  "将路径根据 `dir' 展开
+内部调用了 `expand-file-name'"
+  (let (file-name-handler-alist)
+    (expand-file-name path dir)))
+(defun yynt-fjoin (&rest args)
+  "根据参数合成路径
+若某个参数为绝对路径，那么它会舍弃绝对路径前的参数"
+  (let (path
+        (relative (yynt-frelap (car args))))
+    (mapc
+     (lambda (arg)
+       (setq path (cond ((not path) arg)
+                        ((yynt-fabsp arg)
+                         (progn
+                           (setq relative nil)
+                           arg))
+                        (t (yynt-fexpand arg path)))))
+     args)
+    (if relative (yynt-frela path) path)))
+(defun yynt-fbase (path)
+  "返回 PATH 的文件名，不含扩展名和目录"
+  (file-name-base path))
+(defun yynt-fsame-p (path-a path-b)
+  "若两个目录指向同一文件则返回 t"
+  (equal (file-truename (directory-file-name (yynt-fexpand path-a)))
+	 (file-truename (directory-file-name (yynt-fexpand path-b)))))
+(defun yynt-fname (path)
+  "返回文件名
+对目录也适用，可以返回路径的最后一个目录"
+  (file-name-nondirectory (directory-file-name path)))
+(defun yynt-fdir (path)
+  "返回目录名，不带 /
+对目录也适用，返回除最后目录外的其他部分"
+  (let ((parent (file-name-directory
+                 (directory-file-name (yynt-fexpand path default-directory)))))
+    (unless (yynt-fsame-p path parent)
+      (if (yynt-frelap path)
+          (yynt-frela parent)
+        (directory-file-name parent)))))
+(defun yynt-fno-ext (path)
+  "返回不含扩展名的文件路径"
+  (file-name-sans-extension path))
+(defun yynt-fswap-ext (path ext)
+  "替换文件扩展名"
+  (cl-assert (and (stringp ext) (not (string= "" ext))))
+  (concat (yynt-fno-ext path) "." ext))
+
+;; 一些基于 f.el 的函数扩充
+(defun yynt-get-fullpath (path)
+  "根据相对于根目录的路径获取绝对路径"
+  (yynt-fjoin yynt-basedir path))
+
+(defun yynt-get-cdpath (s-path t-path)
+  "计算从 `s-path' cd 到 `t-path' 所需的路径
+要求为绝对路径，不含文件名"
+  (yynt-frela t-path s-path))
+
+(defun yynt-rela-cdpath (s-path t-path)
+  "s-path 和 t-path 会使用 `yynt-get-fullpath' 扩充然后调用 `yynt-get-cdpath'
+不要含文件名"
+  (yynt-get-cdpath
+   (yynt-get-fullpath s-path)
+   (yynt-get-fullpath t-path)))
+
+;;; 一些 html 代码生成函数
+(defun yynt-html-gen-css (path)
+  "生成 css 引用"
+  (format "<link rel=\"stylesheet\" type=\"text/css\" href=\"%s\">" path))
+(defun yynt-html-gen-ico (path)
+  "生成 icon 引用"
+  (format "<link rel=\"icon\" type=\"image/x-icon\" href=\"%s\">" path))
+(defun yynt-html-gen-js (path &optional async)
+  "生成 javascript 文件引用"
+  (format "<script %ssrc=\"%s\"></script>"
+	  (if async "async " "") path))
+(defun yynt-html-gen-img (path &optional alt)
+  "生成图片引用"
+  (format "<img src=\"%s\" alt=\"%s\">"
+	  path (or alt "Load failed")))
+(defun yynt-html-gen-a (link content &optional others)
+  "生成链接"
+  (format "<a href=\"%s\" %s>%s</a>"
+	  link (or others "") content))
+
+(defun yynt-html-google-font-roboto ()
+  "生成 google 的 roboto 字体引用"
   "\
-<hr>
+<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">
+<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>
+<link href=\"https://fonts.googleapis.com/css2?family=Roboto&display=swap\" rel=\"stylesheet\">")
+
+(defun yynt-html-concat-newline (&rest args)
+  "将所有字符串连接起来"
+  (mapconcat 'identity args "\n"))
+
+(defvar yynt--rela-current-path nil
+  "用于生成相对路径的临时变量")
+(defun yynt-html-r (t-dir name &optional prefix)
+  "生成到 `t-dir' 中的 `name' 文件的相对路径，以 `yynt--rela-current-path' 为基准"
+  (cl-assert (stringp yynt--rela-current-path))
+  (yynt-fjoin (or prefix "")
+	      (yynt-rela-cdpath yynt--rela-current-path
+				t-dir)
+	      name))
+
+;;; post
+;; 博客的发表目录，为二层结构，即 posts/*/index.org
+(defvar yynt-sb-post "posts"
+  "子构建 posts 的相对目录名")
+
+(defun yynt-post-postamble (_info)
+  "用于 post 文章的 postamble"
+  "<hr>
 <div id=\"cc-container\">
 <div>
 <p>Created: %d</p>
@@ -53,19 +194,9 @@
 <img alt=\"CC-BY-SA 4.0\" src=\"../../img/by-sa.svg\"></a>
 </div>")
 
-(defun yynt-post-head (_info)
-  "\
-<link rel=\"stylesheet\" type=\"text/css\" href=\"../../css/style.css\">
-<link rel=\"icon\" type=\"image/x-icon\" href=\"../../img/rin.ico\">
-<script src=\"../../js/copycode.js\"></script>
-<script src=\"../../js/img_hideshow.js\"></script>
-<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">
-<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>
-<link href=\"https://fonts.googleapis.com/css2?family=Roboto&display=swap\" rel=\"stylesheet\">")
-
 (defun yynt-post-index-postamble (_info)
-  "\
-<hr>
+  "用于 post index 的 postamble"
+  "<hr>
 <div id=\"cc-container\">
 <div>
 <p>Updated: %C</p>
@@ -76,19 +207,55 @@
 <img src=\"../img/sink_white.png\" alt=\"⑨\">
 </div>")
 
+(defun yynt-post-head (_info)
+  "生成用于 post 中文章的 http <head>"
+  (let ((yynt--rela-current-path yynt-sb-post))
+    (yynt-html-concat-newline
+     (yynt-html-gen-css (yynt-html-r yynt-d-css "style.css" ".."))
+     (yynt-html-gen-ico (yynt-html-r yynt-d-img "rin.ico" ".."))
+     (yynt-html-gen-js (yynt-html-r yynt-d-js "copycode.js" ".."))
+     (yynt-html-gen-js (yynt-html-r yynt-d-js "img_hideshow.js" ".."))
+     (yynt-html-google-font-roboto))))
+
+(defun yynt-post-index-head (_info)
+  "生成用于 post index 的 http <head>"
+  (let ((yynt--rela-current-path yynt-sb-post))
+    (yynt-html-concat-newline
+     (yynt-html-gen-css (yynt-html-r yynt-d-css "style.css"))
+     (yynt-html-gen-ico (yynt-html-r yynt-d-img "rin.ico"))
+     (yynt-html-google-font-roboto))))
+
 ;;; homepage
 (defun yynt-homepage-postamble (_info)
+  "用于主页的 postamble"
   "<hr><p style=\"text-align:center;\"><i>homepage ends here~</i></p><br>")
 
+;;; repost
+;; repost 发表目录，二层结构，republish/*/index.org
+(defvar yynt-sb-repost "republish"
+  "repost 的目录")
+
 (defun yynt-repost-home/up-func (_info)
+  "用于 repost 的 home/up 元素生成函数"
   "<div id=\"org-div-home-and-up\">
 <a href=\"../index.html\"> HOME </a>
 </div>\n")
 
-;;; repost
+;; 直接使用 post 的 index head 生成
+(defalias 'yynt-repost-index-head 'yynt-post-index-head)
+
+(defun yynt-repost-head (_info)
+  "生成用于 repost 中文章的 http <head>"
+  (let ((yynt--rela-current-path yynt-sb-repost))
+    (yynt-html-concat-newline
+     (yynt-html-gen-css (yynt-html-r yynt-d-css "style.css" ".."))
+     (yynt-html-gen-ico (yynt-html-r yynt-d-img "rin.ico" ".."))
+     (yynt-html-gen-js (yynt-html-r yynt-d-js "copycode.js" ".."))
+     (yynt-html-google-font-roboto))))
+
 (defun yynt-repost-index-postamble (_info)
-  "\
-<hr>
+  "repost index 页面的 postamble"
+  "<hr>
 <div id=\"cc-container\">
 <div>
 <p>Updated: %C</p>
@@ -98,8 +265,8 @@
 ")
 
 (defun yynt-repost-postamble (_info)
-  "\
-<hr>
+  "repost 文章的 postamble"
+  "<hr>
 <div>
 <p>Author: %a</p>
 <p>Created: %d</p>
@@ -108,7 +275,12 @@
 </div>")
 
 ;;; euler
+;; euler 发表目录，一层结构，projecteuler/*.org
+(defvar yynt-sb-euler "projecteuler"
+  "euler 文章的相对路径")
+
 (defun yynt-euler-postamble (_info)
+  "euler 文章的 postamble"
   "\
 <hr>
 <div id=\"cc-container\">
@@ -121,18 +293,23 @@
 <img alt=\"CC-BY-SA 4.0\" src=\"../img/by-sa.svg\"></a>
 </div>")
 
+;; 直接使用 repost index 的 postamble
+(defalias 'yynt-euler-index-postamble 'yynt-repost-index-postamble)
+;; 直接使用 repost index 的 home/up
+(defalias 'yynt-euler-home/up-func 'yynt-repost-home/up-func)
+
 (defun yynt-euler-head (_info)
-  "\
-<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/style.css\">
-<link rel=\"icon\" type=\"image/x-icon\" href=\"../img/rin.ico\">
-<script src=\"../js/copycode.js\"></script>
-<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">
-<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>
-<link href=\"https://fonts.googleapis.com/css2?family=Roboto&display=swap\" rel=\"stylesheet\">")
+  "用于 euler 文章和 index 的 <head>"
+  (let ((yynt--rela-current-path yynt-sb-euler))
+    (yynt-html-concat-newline
+     (yynt-html-gen-css (yynt-html-r yynt-d-css "style.css"))
+     (yynt-html-gen-ico (yynt-html-r yynt-d-img "rin.ico"))
+     (yynt-html-gen-js  (yynt-html-r yynt-d-js  "copycode.js"))
+     (yynt-html-google-font-roboto))))
+
 ;;; 404
 (defun yynt-404-postamble (_info)
   "<hr><div style=\"text-align:center;\"><i>404~</i></div>")
-
 
 ;;; posts start here
 (defun yynt-get-file-info (filename infos)
@@ -762,3 +939,21 @@ https://pe-cn.github.io/%s
   (yynt-publish-global-resource)
   (yynt-publish-current-file
    (file-name-concat yynt-basedir "index.org")))
+
+;; https://sachachua.com/blog/2023/01/adding-a-custom-header-argument-to-org-mode-source-blocks-and-using-that-argument-during-export/
+;; (setq org-babel-exp-code-template "#+begin_src %lang :summary %summary\n%body\n#+end_src")
+;; (defun t-org-html-src-block (src-block _contents info)
+;;   (let* ((result (org-html-src-block src-block _contents info))
+;;          (block-info
+;;           (org-with-point-at (org-element-property :begin src-block)
+;;             (org-babel-get-src-block-info)))
+;;          (summary (assoc-default :summary (elt block-info 2))))
+;;     (if (member summary '("%summary" ""))
+;;         result
+;;       (format "<details><summary>%s</summary>%s</details>"
+;;               summary
+;;               result))))
+;; (with-eval-after-load 'ox-html
+;;   (map-put!
+;;    (org-export-backend-transcoders (org-export-get-backend 'html))
+;;    'src-block 't-org-html-src-block))
