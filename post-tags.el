5a8da5503a3ec7df9bcb72bfaf26462bb554d300
include-yy <969041171@qq.com>
2023-09-10 16:49:31 +0900

    add new tag: robot

--- post-tags.el
+++ post-tags.el
@@ -1,1 +1,1 @@
-("math" "c" "win32" "lisp" "gossip" "git" "emacs" "elisp" "blog" "rescript" "tr" "orgmode")
+("robot" "math" "c" "win32" "lisp" "gossip" "git" "emacs" "elisp" "blog" "rescript" "tr" "orgmode")
