3f5dc66d07202758aa4f0214c09a2569a3f099b0
include-yy <969041171@qq.com>
2024-03-19 20:56:48 +0900

    remove unused code

--- ox-yyh5.el
+++ ox-yyh5.el
@@ -1544,38 +1544,6 @@ holding export options."
 			     attributes)))
     (format "<a%s>%s</a>" attributes (or desc ""))))
 
-;;;; Todo
-
-(defun t--todo (todo info)
-  "Format TODO keywords into HTML."
-  (when todo
-    (format "<span class=\"%s %s%s\">%s</span>"
-	    (if (member todo org-done-keywords) "done" "todo")
-	    (or (plist-get info :html-todo-kwd-class-prefix) "")
-	    (t-fix-class-name todo)
-	    todo)))
-
-;;;; Priority
-
-(defun t--priority (priority _info)
-  "Format a priority into HTML.
-PRIORITY is the character code of the priority or nil.  INFO is
-a plist containing export options."
-  (and priority (format "<span class=\"priority\">[%c]</span>" priority)))
-
-;;;; Tags
-
-(defun t--tags (tags info)
-  "Format TAGS into HTML.
-INFO is a plist containing export options."
-  (when tags
-    (format "<span class=\"tag\">%s</span>"
-	    (mapconcat
-	     (lambda (tag)
-	       (format "<span>%s</span>"
-		       tag))
-	     tags "&#xa0;"))))
-
 ;;;; Src Code
 
 (defun t-fontify-code (code lang)
@@ -1709,19 +1677,16 @@ of contents as a string, or nil if it is empty."
 			 (org-export-get-relative-level headline info)))
 		 (org-export-collect-headlines info depth scope))))
     (when toc-entries
-      (let ((toc (concat "<div id=\"text-table-of-contents\" role=\"doc-toc\">"
-			 (t--toc-text toc-entries)
-			 "</div>\n")))
+      (let ((toc (t--toc-text toc-entries)))
 	(if scope toc
-	  (let ((outer-tag "nav"))
-	    (concat (format "<%s id=\"table-of-contents\" role=\"doc-toc\">\n" outer-tag)
-		    (let ((top-level (plist-get info :html-toplevel-hlevel)))
-		      (format "<h%d>%s</h%d>\n"
-			      top-level
-			      "Table of Contents"
-			      top-level))
-		    toc
-		    (format "</%s>\n" outer-tag))))))))
+	  (concat "<nav id=\"toc\">\n"
+		  (let ((top-level (plist-get info :html-toplevel-hlevel)))
+		    (format "<h%d>%s</h%d>\n"
+			    top-level
+			    "Table of Contents"
+			    top-level))
+		  toc
+		  "</nav>\n"))))))
 
 (defun t--toc-text (toc-entries)
   "Return innards of a table of contents, as a string.
@@ -1751,18 +1716,10 @@ and value is its relative level, as an integer."
   "Return an appropriate table of contents entry for HEADLINE.
 INFO is a plist used as a communication channel."
   (let* ((headline-number (org-export-get-headline-number headline info))
-	 (todo (and (plist-get info :with-todo-keywords)
-		    (let ((todo (org-element-property :todo-keyword headline)))
-		      (and todo (org-export-data todo info)))))
-	 (todo-type (and todo (org-element-property :todo-type headline)))
-	 (priority (and (plist-get info :with-priority)
-			(org-element-property :priority headline)))
 	 (text (org-export-data-with-backend
 		(org-export-get-alt-title headline info)
 		(org-export-toc-entry-backend 'html)
-		info))
-	 (tags (and (eq (plist-get info :with-tags) t)
-		    (org-export-get-tags headline info))))
+		info)))
     (format "<a href=\"#%s\">%s</a>"
 	    ;; Label.
 	    (t--reference headline info)
